d9eb420c1bd0ee7cf50abf1a579aa5086de8bea2
Elie Steinbock <eliesteinbock@gmail.com>
2017-12-03 15:19:14 +0200

    added ability to attach users to referrer accounts

--- server/addReferrer.js
+++ server/addReferrer.js
@@ -1,3 +1,6 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { check } from 'meteor/check';
 
 Meteor.methods({
   addReferrer({ code, name, url, commissionRate, cpa }) {
@@ -16,6 +19,31 @@ Meteor.methods({
       url,
       commissionRate,
       cpa,
+    });
+  },
+  attachReferrerToEmail({ email, code }) {
+    check(email, String);
+
+    if (!Roles.userIsInRole(this.userId, ['admin']))
+      throw new Meteor.Error(404, 'Must be admin to call this method', this.userId);
+
+    const user = Accounts.findUserByEmail(email);
+
+    if (!user)
+      throw new Meteor.Error('error', 'Unable to find user.', email);
+
+    Referrers.update({ code }, {
+      $addToSet: {
+        userIds: user._id,
+      },
+    }, (err) => {
+      if (!err) {
+        Meteor.users.update({ _id: user._id }, {
+          $set: {
+            isAffiliate: true,
+          },
+        }, () => {});
+      }
     });
   },
 });
